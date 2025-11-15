72949ffba89ab5c5d89232856eb151e639370c2a
asdawdsfaf <hostprov7@gmail.com>
2025-11-15 21:40:43 +0200

    Update inline_method.py

--- inline_method.py
+++ inline_method.py
@@ -1,22 +1,22 @@
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
+# inline_method.py
 
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 button_1 = KeyboardButton('NFT 🎆')
 button_2 = KeyboardButton('Личный кабинет 📁')
 button_3 = KeyboardButton('Информация ℹ️')
 button_4 = KeyboardButton('🧑‍💻 Поддержка')
-greet_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_1).add(button_2).row(button_3, button_4)
 
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_1).add(button_2).row(button_3, button_4)
 
 menu_kb = InlineKeyboardMarkup()
 inline_btn_1 = InlineKeyboardButton('📥 Пополнить', callback_data='invest')
 inline_btn_2 = InlineKeyboardButton('Вывести 📤', callback_data='un_invest')
 inline_btn_3 = InlineKeyboardButton('🖼 Мои NFT 🖼', callback_data='my_nft')
 inline_btn_4 = InlineKeyboardButton('💰 Сменить валюту 💰', callback_data='change_wallet')
+inline_btn_5 = InlineKeyboardButton('🔗 Привязать NFT из Telegram', callback_data='link_tg_nft')
 
-menu_kb.row(inline_btn_1, inline_btn_2).add(inline_btn_3).add(inline_btn_4)
+menu_kb.row(inline_btn_1, inline_btn_2).add(inline_btn_3).add(inline_btn_4).add(inline_btn_5)
 
 admin_kb = InlineKeyboardMarkup()
 inline_btn_1 = InlineKeyboardButton('🌟 Добавить коллекцию 🌟', callback_data='add_collection')
@@ -25,26 +25,26 @@ inline_btn_3 = InlineKeyboardButton('🌟 Найти мамонта по ID 🌟
 admin_kb.add(inline_btn_1).add(inline_btn_2).add(inline_btn_3)
 
 invest = InlineKeyboardMarkup()
-inline_btn_1 = InlineKeyboardButton('🌟 CARD [RU] 🌟', callback_data='plus_card')
-inline_btn_2 = InlineKeyboardButton('🌟 USDT 🌟', callback_data='plus_usdt')
-invest.add(inline_btn_1).add(inline_btn_2)
+invest.add(
+    InlineKeyboardButton('🌟 CARD [RU] 🌟', callback_data='plus_card'),
+    InlineKeyboardButton('🌟 USDT 🌟', callback_data='plus_usdt')
+)
 
 un_invest = InlineKeyboardMarkup()
-inline_btn_1 = InlineKeyboardButton('🌟 CARD 🌟', callback_data='un_card')
-inline_btn_2 = InlineKeyboardButton('🌟 CRYPTO 🌟', callback_data='un_crypto')
-un_invest.add(inline_btn_1).add(inline_btn_2)
+un_invest.add(
+    InlineKeyboardButton('🌟 CARD 🌟', callback_data='un_card'),
+    InlineKeyboardButton('🌟 CRYPTO 🌟', callback_data='un_crypto')
+)
 
 mamont = InlineKeyboardMarkup()
-inline_btn_1 = InlineKeyboardButton('🦣 Мои мамонты 🦣', callback_data='my_mamont')
-mamont.add(inline_btn_1)
-
+mamont.add(InlineKeyboardButton('🦣 Мои мамонты 🦣', callback_data='my_mamont'))
 
 change = InlineKeyboardMarkup()
-inline_btn_1 = InlineKeyboardButton('🇷🇺 RUB', callback_data='change_RUB')
-inline_btn_2 = InlineKeyboardButton('🇺🇦 UAH', callback_data='change_UAH')
-inline_btn_3 = InlineKeyboardButton('🇺🇸 USD', callback_data='change_USD')
-inline_btn_4 = InlineKeyboardButton('🇪🇺 EUR', callback_data='change_EUR')
-inline_btn_5 = InlineKeyboardButton('🇵🇱 PLN', callback_data='change_PLN')
-inline_btn_6 = InlineKeyboardButton('🇧🇾 BLN', callback_data='change_BLN')
-change.add(inline_btn_1).add(inline_btn_2).add(inline_btn_3).add(inline_btn_4).add(inline_btn_5).add(inline_btn_6)
-
+change.add(
+    InlineKeyboardButton('🇷🇺 RUB', callback_data='change_RUB'),
+    InlineKeyboardButton('🇺🇦 UAH', callback_data='change_UAH'),
+    InlineKeyboardButton('🇺🇸 USD', callback_data='change_USD'),
+    InlineKeyboardButton('🇪🇺 EUR', callback_data='change_EUR'),
+    InlineKeyboardButton('🇵🇱 PLN', callback_data='change_PLN'),
+    InlineKeyboardButton('🇧🇾 BLN', callback_data='change_BLN'),
+)
